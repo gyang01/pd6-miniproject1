1277cde165d2a0ac4e2d48266533157c1b0f1594
NJacob <kn2ply@gmail.com>
2012-10-19 22:37:39 -0400

    not done yet

--- Daelin-Nevin/mongo.py
+++ Daelin-Nevin/mongo.py
@@ -2,5 +2,34 @@
 from pymongo import Connection
 
 con = Connection("mongo.stuycs.org")
-db = con.admin
-res = db.authenticate("ml7","ml7")
+
+global db
+global res
+global col
+
+def startup():
+	db = con.admin
+	res = db.authenticate("ml7","ml7")
+	db = con["z-pd6"]
+	col = db["DaNe"]
+
+def addstory(name):
+	story = {"name": name, "line":"0", "text":name}
+	db.insert(story)
+
+def addline(story, line):
+	res = col.find()
+	ln = 0
+	for entry in res:
+		if entry["name"] == story:
+			ln+= 1
+	newline = {"name":story, "line":str(ln), "text":line}
+	db.insert(newline)
+
+def get_stories():
+	ret = [];
+	res = col.find()
+	for entry in ret:
+		if entry["line"] == "0":
+			ret.append(entry)
+	return ret
