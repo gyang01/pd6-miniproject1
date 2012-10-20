4f61df47776b5faf2f8b26c60470c1aba7b3a791
Sarah Robinson <robinsonsarah01@gmail.com>
2012-10-19 20:39:55 -0400

    playing with util

--- Li-Robinson/util.py
+++ Li-Robinson/util.py
@@ -1,15 +1,36 @@
 from pymongo import Connection
 
 conn = Connection('mongo.stuycs.org')
-#db = ''
+global db
+global collection
 
 def auth():
+    global db
     db = conn.admin
     res = db.authenticate('ml7','ml7')
 
 def start(): #or init
     auth()
     db = conn['Li-Robinson-mp1'] #name of database for this project
+    global collection
+    collection = db['stories']
     
 def add_story(storystart):
-    db[storystart][storystart] = storystart #first line of story is story name
+   # global collection
+    #collection = db['stories']
+    collection.insert({'lines':[storystart]}) #first line of story is story name
+
+def get_story(storyname):
+    #global collection
+    #collection = db['stories']
+    for thing in collection.find():
+        print thing['lines']
+
+#test
+if __name__ == "__main__":
+    start()
+    global collection
+    collectino = db['There once was a']
+    collection.drop()
+    #add_story('There once was a')
+    #print get_story('There once was a')
