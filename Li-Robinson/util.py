be9de2d7b8d93edb0dc51c473b1332b5671c3c64
Sarah Robinson <robinsonsarah01@gmail.com>
2012-10-19 22:00:39 -0400

    working util.py

--- Li-Robinson/util.py
+++ Li-Robinson/util.py
@@ -1,41 +1,45 @@
 from pymongo import Connection
 
 conn = Connection('mongo.stuycs.org')
-global db
-global collection
 
 def auth():
-    global db
     db = conn.admin
     res = db.authenticate('ml7','ml7')
 
 def start(): #or init
     auth()
-    db = conn['Li-Robinson-mp1'] #name of database for this project
-    global collection
-    collection = db['stories'] #name of collection of stories
-    
+    #db = conn['Li-Robinson-mp1'] #name of database for this project
+        
 def add_story(storystart):
+    db = conn['Li-Robinson-mp1']
+    collection = db['stories']
+    #if collection.find({'name':storystart}) == None: #try to check if the story exists
     collection.insert({'name':storystart, 'lines':[storystart]}) #first line of story is story title, and its key is its title; its lines are a list
 
 def get_story(story):
+    db = conn['Li-Robinson-mp1']
+    collection = db['stories']
     res = collection.find_one({'name':story})
     if res:
         return res['lines']
 
 def add_line(story,line):
+    db = conn['Li-Robinson-mp1']
+    collection = db['stories']
     res = collection.find_one({'name':story})
-    #lines = res['lines']
-    print res#['lines']
-    #lines.append(line)
-    #collection.update({'name':story},{ '$set': {'lines':lines} })
+    if res:
+        lines = res['lines']
+        lines.append(line)
+        collection.update({'name':story},{ '$set': {'lines':lines} })
 
 #test
 if __name__ == "__main__":
-    story = 'There once was a'
-    start()
-    for line in db.collection.find(): print line
-    #print db.collection_names()
-    #add_story('There once was a')
-    #add_line(story,'Man named Fred')
-    #print get_story(story)
+    story = 'Once upon a time'
+    auth()
+    db = conn['Li-Robinson-mp1']
+    collection = db['stories']
+    #add_story(story)
+    #add_line(story,'In a land far far away')
+    print db.collection_names()
+    for line in collection.find(): print line
+    print "done"
