e491a126e6fbe98668dd1cb185406247c4c57929
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-10-21 12:21:53 -0400

    started util.py

---
+++ Dina-Wilson/util.py
@@ -0,0 +1,18 @@
+from pymongo import Connection
+global connection
+global db
+global res
+global col
+
+def auth():
+    global connection
+    global db
+    global res
+    global col
+    connection = Connection('mongo.stuycs.org')
+    db = connection.admin
+    res = db.authenticate ('ml7', 'ml7')
+    db = connection['z-pd6']
+    col = db['DLWS']
+
+def 
