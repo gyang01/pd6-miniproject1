9ee4478b2e92afd110472abee0e1892b3487a11a
gyang01 <georgiana.yang@yahoo.com>
2012-10-21 19:02:17 -0300

    merged

--- Georgiana-Victoria/db.py
+++ Georgiana-Victoria/db.py
@@ -15,7 +15,16 @@ def add_line(title="Example", line="a"):
         add_story(title)
     else:
         stories.update({'title':str(title)}, {"$push": {'lines':str(line)} } )
-<<<<<<< HEAD
+
+def add_story(title="Example"):
+    if isTitle(title)<1:
+        story={"title":str(title),'lines':[]}
+        stories.insert(story)
+    else:
+        pass
+
+def isTitle(title="Example"):
+    return stories.find({'title':str(title)}).count()
 
 def getTitles():
     res = stories.find()
@@ -40,41 +49,11 @@ def getAll():
         print line
 
 
-=======
->>>>>>> cce95e694faf29ea6da47197eed254d1787c9069
-
-def add_story(title="Example"):
-    if isTitle(title)<1:
-        story={"title":str(title),'lines':[]}
-        stories.insert(story)
-    else:
-        pass
-
-def isTitle(title="Example"):
-    return stories.find({'title':str(title)}).count()
-
 #stories.drop()
 def test():
-<<<<<<< HEAD
-    add_story('Example', 'a')
-
-
-=======
     add_story('Example')
     #add_line('Example','a')
->>>>>>> cce95e694faf29ea6da47197eed254d1787c9069
 test()
-res = stories.find()
-for line in res:
-    print line
-def getTitles():
-    l=[]
-    res = stories.find()
-    for r in res:
-        l.append(r['title'])
-    return l
-print getTitles()
-<<<<<<< HEAD
 getAll()
 print 'After test'
 remove_story('Example')
@@ -83,7 +62,4 @@ getAll()
 print getTitles()
 getAll()
 #print getLines()
-=======
 
-
->>>>>>> cce95e694faf29ea6da47197eed254d1787c9069
