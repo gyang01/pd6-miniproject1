9c8858c38127fc835328585b782858a6bc7e6200
Daniel Teehan <drteehan@gmail.com>
2012-10-20 23:04:03 -0400

    first functions outlined

--- So Teehan/storinator.py
+++ So Teehan/storinator.py
@@ -3,3 +3,13 @@ Connection=Connection('mongo.stuycs.org')
 db = Connection.admin
 res=db.authenticate('ml7','ml7')
 db = Connection['SoTe-pd6']
+
+def addstory(story):
+    db.stories.save({story:['string']})
+
+def addcontent(story, line):
+
+
+addstory('the worst thing ever invented')
+addstory('i dont know really')
+addstory('arms and a man i sing')
