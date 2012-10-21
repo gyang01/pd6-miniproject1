4178aa4c7c52a4ff503f30048ba2ab9e7ad3738f
Patrick So <bassicozz@gmail.com>
2012-10-21 19:38:50 -0400

    final commit

--- So Teehan/storinator.py
+++ So Teehan/storinator.py
@@ -3,6 +3,7 @@ Connection=Connection('mongo.stuycs.org')
 db = Connection.admin
 res=db.authenticate('ml7','ml7')
 db = Connection['SoTe-pd6']
+<<<<<<< HEAD
 storycontent = ''
 
 def addstory(story):
@@ -12,8 +13,35 @@ def addcontent(story, line):
     for line in res:
         if line['story'] == story
             line['string'] = line['string' + line]
+=======
+db.stories.remove()
+def addstory(story):
+    entry= {"story":story, "text":[]}
+    db.stories.save(entry)
+
+
+>>>>>>> updated addcontent
     
 
 addstory('the worst thing ever invented')
 addstory('i dont know really')
 addstory('arms and a man i sing')
+
+def addcontent(story, line):
+    for valjean in db.stories.find({'story':story}):
+        array = valjean['text']
+        array.append([line])
+        db.stories.update({'story':story}, {'story': story,'text':array})
+
+addcontent('the worst thing ever invented', 'was facebook')
+addcontent('the worst thing ever invented', 'because of the procrastination')
+for line in db.stories.find():
+            print str(line)
+ #   db.stories.update({story
+#for line in db.stories.find():
+ #   print line
+
+#for line in db.stories.find({"story":'the worst thing ever invented'}):
+ #               print str(line)
+  #              print str(line['text'])
+  
