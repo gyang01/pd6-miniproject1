90680ec009d77617d8900756dbcfec4f8a78b4a2
gyang01 <georgiana.yang@yahoo.com>
2012-10-21 17:23:04 -0400

    has add_line and add_story and connects to the database

--- Georgiana-Victoria/db.py
+++ Georgiana-Victoria/db.py
@@ -10,17 +10,24 @@ db = Connection['Georgiana-Victoria']
 
 stories=db.story_collection
 titles=[]
-def add_story(title="Example", line="a"):
-    if not title in titles:
+
+def add_line(title="Example", line="a"):
+    if not title in getTitles():
+        add_story(title)
+    else:
+        stories.update({'title':str(title)}, {"$push": {'lines':str(line)} } )
+
+def add_story(title="Example"):
+    if not title in getTitles():
         titles.append(str(title))
         lines=[str(line)]
         story={'title':str(title),'lines':str(lines)}
         stories.insert(story)
-    else:
-        stories.update({'title':str(title)}, {"$push": {'lines':str(line)} } )
+
 def getTitles():
     return titles
+
 #res = stories.find()
 #for line in res:
 #    print line
-print getTitles()
+#print getTitles()
