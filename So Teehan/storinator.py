892b2b9c75a70dcd4410dd173c5119510525f422
Patrick So <bassicozz@gmail.com>
2012-10-20 23:40:21 -0400

    new stuff
    not sure if its right

--- So Teehan/storinator.py
+++ So Teehan/storinator.py
@@ -3,12 +3,14 @@ Connection=Connection('mongo.stuycs.org')
 db = Connection.admin
 res=db.authenticate('ml7','ml7')
 db = Connection['SoTe-pd6']
+storycontent = ''
 
 def addstory(story):
     db.stories.save({story:['string']})
 
 def addcontent(story, line):
-
+    #collection.find_one({story})
+    #db.stories.storycontent = db.stories.storycontent + '\n' +line
 
 addstory('the worst thing ever invented')
 addstory('i dont know really')
