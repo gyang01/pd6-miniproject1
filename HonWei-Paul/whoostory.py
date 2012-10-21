5f66fb9147b7aa148c9810a08079604be4f3c715
Honz <honwei95@gmail.com>
2012-10-21 17:51:46 -0400

    homework
    herpaderpderp i hope paul does html stuff

---
+++ HonWei-Paul/whoostory.py
@@ -0,0 +1,32 @@
+from pymongo import Connection
+c = Connection('mongo.stuycs.org')
+db = c.admin
+res=db.authenticate('ml7','ml7')
+db = c['KhorMa-pd6']
+stories = db.storynator
+
+def addstory(name):
+    if not exists(name):
+        stories.save({'Title':name,'Storytext':[]})
+        
+def exists(name):
+    return str(stories.find_one({'Title':name}))!='None'
+
+def getstory(name):
+    return stories.find_one({'Title':name})
+
+def getstorytext(name):
+    a = getstory(name)[u'Storytext']
+    for b in range(0,len(a)):
+	a[b] = str(a[b])
+    return a
+
+def addline(name,line):
+    stories.update({'Title':name}, {'$push': {'Storytext':line}})
+
+def getalltitles():
+    a = stories.find()
+    b = []
+    for d in a:
+        b.append(str(d[u'Title']))
+    return b
