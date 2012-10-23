e60dc7f44149ee6652d3de71bd6d6f2f35dc8d3a
gyang01 <georgiana.yang@yahoo.com>
2012-10-23 18:42:44 -0400

    added to app.py and home.html

--- Georgiana-Victoria/app.py
+++ Georgiana-Victoria/app.py
@@ -2,8 +2,28 @@ from flask import Flask
 from flask import request, make_response
 from flask import render_template
 import db
-from flask import url_for,redirect,flash, session, escape
+from flask import url_for,redirect,flash, escape
 
 app = Flask(__name__)
 
-
+@app.route("/",methods=['GET','POST'])
+def home():
+    if request.method=="GET":
+        return render_template("webage.html")
+    else:
+        button=request.form['button']
+        if button=='Create!':
+            title=request.form['ntitle']
+            db.add_story(title)
+            return redirect(url_for(home))
+        elif:
+            if button=='Read!':
+                otitle=request.form['otitle']
+                resp=make_response(render_template("webpage.html"))
+                resp.set_cookie('title', otitle)
+                return redirect(url_for(story))
+            return redirect(url_for(home))
+            
+if __name__=="__main__":
+    app.debug=True
+    app.run()
