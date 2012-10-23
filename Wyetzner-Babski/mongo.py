b0cc69f7f73ccd4f5977b962ec93f2ec9610aa3a
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2012-10-23 18:14:49 -0400

    Redid class system in mongo.py

--- Wyetzner-Babski/mongo.py
+++ Wyetzner-Babski/mongo.py
@@ -1,19 +1,37 @@
 from pymongo import Connection
 
-Connection = Connection('mongo.stuycs.org')
+class mongo:
+    def __init__(self):
+        self.connection = Connection('mongo.stuycs.org')
+        self.db = self.connection.admin
+        self.db.authenticate('ml7','ml7')
+        self.db = self.connection['Sofia-Sarah']
+        self.db.stories.drop()
 
-db = Connection.admin
-res=db.authenticate('ml7','ml7')
+    def newStory(self, title):
+        self.db.stories.insert({'title':title,'lines':[]})
 
-db = Connection['SofiaSarah']
+    def getStories(self):
+        return [x['title'] for x in self.db.stories.find()]
 
-collection = db['stories']
+    def newLine(self,title,line):
+        s = self.db.stories
+        story = [x for x in s.find({'title':title})]
+        if len(story) != 1:
+            return
+        l = story[0]
+        l['lines'].append(line)
+        s.update({'title':title},l)
 
-def addStory(title):
-    collection.insert( {'title': title, 'lines': [title] } )
+    def getLines(self,title):
+        return self.db.stories.find({'title':title})[0]['lines']
 
-def getStory(title):
-    stories = collection.find()
-    for s in stories:
-        if (str(s['title'])) == title:
-            return s
+if __name__ == '__main__':
+    db1 = mongo()
+    db1.newStory("Pride and Prejudice")
+    db1.newStory("The Picture of Dorian Grey")
+    print db1.getStories()
+    db1.newLine("Pride and Prejudice","It is a truth universally acknowledged")
+    print db1.getLines("Pride and Prejudice")
+    
+        
