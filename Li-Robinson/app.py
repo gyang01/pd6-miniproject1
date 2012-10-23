3ef279e26d15ccfacffb84a8e77b5bf7e0b7a005
Sarah Robinson <robinsonsarah01@gmail.com>
2012-10-23 17:02:38 -0400

    working on app.py

--- Li-Robinson/app.py
+++ Li-Robinson/app.py
@@ -1,10 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 import util
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def home():
     util.auth()
-    titles = util.get_story_titles()
-    return render_template("home.html",titles=titles)
+    if request.method == 'GET':
+        titles = util.get_story_titles()
+        return render_template("home.html",titles=titles)
+    else:
+        button = request.form["button"]
+        if button == "Go":
+            pass
+        elif button == "Create":
+            storyname = request.form["new_story"]
+            util.add_story(storyname)
+            titles = util.get_story_titles()
+            return render_template("home.html",titles=titles)
