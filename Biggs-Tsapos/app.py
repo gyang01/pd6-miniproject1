c929c62d32f5bc461c86dd1c2180499a7bea92fd
Nathaniel Biggs <biggs0125@gmail.com>
2012-10-24 19:44:38 -0400

    Renders home page

--- Biggs-Tsapos/app.py
+++ Biggs-Tsapos/app.py
@@ -7,8 +7,13 @@ import util
 app = Flask(__name__)
 
 @app.route('/')
+def start():
+    return redirect(url_for('home'))
+
+@app.route("/home/",methods=['GET','POST'])
 def home():
-    
+    if request.method=='GET':
+        return render_template('home.html')
 
 if __name__ == '__main__':
     app.debug = True
