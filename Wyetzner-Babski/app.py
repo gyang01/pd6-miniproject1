bb7c81b0695a6f5d2d88854d30a7369b126f169d
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2012-10-24 21:33:35 -0400

    Fixed app.py

--- Wyetzner-Babski/app.py
+++ Wyetzner-Babski/app.py
@@ -7,17 +7,29 @@ import mongo
 app = Flask(__name__)
 app.secret_key = 'super_secret'
 
+m = mongo.mongo()
+#selStory =''
+
 @app.route("/", methods=['GET','POST'])
 def default():
     if request.method=='GET':
+        stories = mongo.mongo.getStories(m)
+        return render_template("default.html", stories=stories)
+    else:
+#        s = str(request.form['story'])
+        ns = str(request.form['newStory'])
+    
+    b = request.form['button']
+    if b=='Choose Story':
         return render_template("default.html")
-    if request.method=='POST':
-        button=request.form['button']
-        if button=='Choose Story':
-            return render_template("default.html")
-        if button=='Create Story':
-            mongo.newstory(str(request.form["newstory"]))
-            return render_template("default.html")
+    elif b == "Create Story":
+        if not ns:
+            return redirect(url_for('default'))
+        else:
+            mongo.mongo.newStory(m,ns)
+            return redirect(url_for('default'))
+
+
         
 
 if __name__ == "__main__":
