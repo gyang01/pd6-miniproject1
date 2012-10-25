c86315b83c5398b42e3c3bdc94d82ae1526563aa
Patrick So <bassicozz@gmail.com>
2012-10-25 00:00:39 -0400

    update

--- So Teehan/app.py
+++ So Teehan/app.py
@@ -6,18 +6,14 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 @app.route("/", methods = ["GET", "POST"])
 def home():
-	if request.method == "GET":
-	 return render_template("homeminiproj.html")
+ if request.method == "GET":
+  return render_template("homeminiproj.html")
+        sorinator.con()
         storinator.addstory(str(request.form["storyname"]))
-        storyarray=[]
-
-        for valjean in db.stories.find():
-         storyarray.append(valjean['story'])
-
-        #button = request.form["button"]
-
+        storyarray= storinator.getstories()
         
-         
-
+        #button = request.form["button"]
+        
+        
         #if button == "go":
          #   return render_template("storypage.html")
--- So Teehan/storinator.py
+++ So Teehan/storinator.py
@@ -1,11 +1,13 @@
 from pymongo import Connection
 def con():
     Connection=Connection('mongo.stuycs.org')
+    global db
+    global res
+    global col
     db = Connection.admin
     res=db.authenticate('ml7','ml7')
     db = Connection['SoTe-pd6']
     db.stories.remove()
-
 def addstory(story):
     entry= {"story":story, "text":[]}
     for x in db.stories.find():
@@ -13,20 +15,17 @@ def addstory(story):
             return
     db.stories.save(entry)
  
-
 def getstories():
     array = []
     for x in db.stories.find():
         array.append(x['story'])
     print array
-
     return array
 def addcontent(story, line):
     for valjean in db.stories.find({'story':story}):
         array = valjean['text']
         array.append(line)
         db.stories.update({'story':story}, {'story': story,'text':array})
-
 
 def access_story(story):
     for javert in db.stories.find({'story':story}):
@@ -37,17 +36,13 @@ def access_story(story):
             mod = str(i)
             ans= ans+ " " + str(i) 
     print ans
-
-
-
+ 
 
 #for line in db.stories.find():
 #            print str(line)
  #   db.stories.update({story
 #for line in db.stories.find():
  #   print line
-
 #for line in db.stories.find({"story":'the worst thing ever invented'}):
  #               print str(line)
   #              print str(line['text'])
-  
