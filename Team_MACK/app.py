165922e9176b38270b5075249464454d66d862a7
Michael Cohen <budjmt@aol.com>
2012-10-25 02:05:46 -0400

    Fixed a couple more things

--- Team_MACK/app.py
+++ Team_MACK/app.py
@@ -1,32 +1,35 @@
 from util import *
 from flask import Flask, render_template, session, url_for, request, escape, redirect, g
+from database import *
 
+#configuration
 DEBUG = True
 
+#init
 app = Flask(__name__)
 app.config.from_object(__name__)
 db = DatabaseConnection()
 
 @app.route("/", methods=["GET", "POST"])
-def ind():
+def index():
     if request.method == "POST":
         if "story_menu" in request.form:
             return redirect(url_for("story_page", story=request.form["story_menu"]))
         else:
             story_name = request.form["add_story_field"]
             if story_name:
-                db.addStory(story_name)
-    return render_template("index.html", stories=db.getStories())
+                addStory(db, story_name)
+    return render_template("index.html", stories=getStories(db))
 
 @app.route("/<story>", methods=["GET", "POST"])
 def storyPage(story):
     if request.method == "POST":
-        db.addLine(story, request.form["add_line_field"])
-    return render_template("story.html", lines=db.getStory(story))
+        addLine(db, story, request.form["add_line_field"])
+    return render_template("story.html", lines=getStory(db, story))
 
 @app.route("/drop/stories")
 def dropPage():
-    db.removeStories()
+    removeStories(db)
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
