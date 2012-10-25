244065e905124d65789f06e1ff6bf1f7e2037dfc
allidoiswin <fallenpwr@gmail.com>
2012-10-24 22:00:10 -0400

    app.py structure

--- WinIsBallin/app.py
+++ WinIsBallin/app.py
@@ -12,6 +12,18 @@ app = Flask(__name__)
 def home():
     if request.method == "GET":
         return render_template('home.html', stuff="stuff")
+    if request.form['button'] == "Create New Story":
+	return redirect(url_for('createStory')
+
+@app.route("/createStory", methods=['POST'])
+def createStory:
+	if request.method == "GET":
+		return render_template('createStory.html')
+	if request.form['button'] == "Create":
+		story.newStory(request.form['title'])
+		return redirect(url_for('home')
+
+
     
 if __name__ == '__main__':
     app.run()
