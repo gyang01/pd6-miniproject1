a718c97ea630ac6575b193fb3bcacf29c4c632bd
Daniel Teehan <drteehan@gmail.com>
2012-10-25 21:29:40 -0400

    updated function con()

--- So Teehan/app.py
+++ So Teehan/app.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template, session, url_for, redirect, request, e
 import os 
 import storinator
 app = Flask(__name__)
+storinator.con()
 app.secret_key = os.urandom(24)
 @app.route("/", methods = ["GET", "POST"])
+
 def home():
     
     
  if request.method == "GET":
   return render_template("homeminiproj.html")
  else:
-        storinator.con()
         storyarray= storinator.getstories()
         button = request.form["button"]
         if button == "create" and len(str(request.form["storyname"])) > 0:
@@ -26,14 +27,14 @@ def storypage():
  if request.method == "GET":
     return render_template("storypage.html")
  else:
-         button2 = request.form["button2"]
-         storycont = storinator.get_story(storypick)
-        if button2 == "Add":
-            newline = str(request.form["newline"])
-            storinator.addcontent(storypick,newline)
-            return render_template("storypage.html")
-        if button2 == "Go Back":
-            return redirect(url_for("home"))
+     button2 = request.form["button2"]
+     storycont = storinator.get_story(storypick)
+     if button2 == "Add":
+         newline = str(request.form["newline"])
+         storinator.addcontent(storypick,newline)
+         return render_template("storypage.html")
+     if button2 == "Go Back":
+         return redirect(url_for("home"))
             
         
 if __name__ == "__main__":
