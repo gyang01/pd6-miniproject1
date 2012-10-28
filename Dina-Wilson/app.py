cf2aa03688cf435c949534f9d4153d984f3d0414
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-10-28 01:37:29 -0400

    changed app

--- Dina-Wilson/app.py
+++ Dina-Wilson/app.py
@@ -3,26 +3,31 @@ from flask import url_for,redirect,flash
 from flask import session, escape
 from flask import request
 from flask import render_template
-import databasing
 from pymongo import connection
 import util
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def begin():
-    global story = ""
-     if request.method=="GET":
-        return render_template("home.html")
-     if request.method=="POST":
+def home():
+    util.auth()
+    global story
+    story = ""
+    if request.method=="GET":
+        return render_template("home.html", titles = util.getAllStoryTitles())
+    if request.method=="POST":
          button = request.form["button"]
          if button == "Ok":
-             story = str(request.form["stories"])
+             story = str(request.form["story"])
              return redirect(url_for("story"))
          if button == "Add":
-             util.addstory(str(request.form["storyname"]))
-             return render_template("home.html")
+             util.addstory(str(request.form["New Story"]))
+             return render_template("home.html", titles = util.getAllStoryTitles())
              
 
 @app.route("/story", methods = ["GET", "POST"])
 def story():
+    pass
+
+if __name__ == "__main__":
+	app.run(debug = True)
