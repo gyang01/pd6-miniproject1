0db01f5d9e3500847c13acc112d5481bec4bf076
Amanda Boss <vamandaboss1@gmail.com>
2012-11-04 15:44:28 -0500

    Update Amanda-Emily/app.py

--- Amanda-Emily/app.py
+++ Amanda-Emily/app.py
@@ -2,28 +2,39 @@ from flask import Flask
 from flask import request, make_response
 from flask import render_template
 from flask import url_for,redirect,flash, escape
-import mongo #not sure if this is right, might be import db
+import mongo
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
 	if request.method=="GET":
-		return render_template("homepage.html")
-	else: #what to do if the user submits a new story
-		button=request.form['button']
+		return render_template("homepage.html", stories = mongo.getAllTitles())
+	if request.method=="POST":
+		button=request.form["button"]
 		if button=='Submit':
-			title=request.form['name']
-			db.add_story(title)
-			return redirect(url_for(home))
-		elif: #what to do if the user picks a story in progress
-			if button=='Go':
-			title=request.form['name'] #not sure if I should refer to this as title/will this cause any conflicts?
-			return redirect(url_for((story))
-		return redirect(url_for(home)) 
+			name=request.form['name']
+			mongo.addstory(name)
+			return render_template("homepage.html", stories = mongo.getAllTitles())
+                elif(button=='Go'): 
+                        name=str(request.form['name'])
+                        return render_template("addstory.html", comments = mongo.getStory(name))
+                
+                        
                        
 	
-	
+@app.route("/", methods=['GET', 'POST'])
+def addstory():
+           if request.method=="POST":
+                   button= request.form['button']
+                   if button=="save":
+                        line = str(request.form["comments"])
+                        name = str(request.form["name"])
+                        mongo.addLine(name, line)
+                        return render_template("addstory.html", name = name, comments = mongo.getStory(name))
+                   if button=="cancel":
+                           return redirect(url_for(home))
+           
 if __name__=="__main__":
 	app.debug=True
 	app.run(port=6000)
