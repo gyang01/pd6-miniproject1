2b680b8f603b319cc4823a04de392bdb349f5287
allidoiswin <fallenpwr@gmail.com>
2012-11-04 22:18:55 -0500

    working although titles cannot have spaces D:

---
+++ WinIsBallin/storyuncl.py
@@ -0,0 +1,45 @@
+from pymongo import Connection
+
+#Comm-link established
+conn = Connection('mongo.stuycs.org')
+
+#I am ADMIN
+db = conn.admin
+AUTHENTICATION = db.authenticate('ml7','ml7')
+#Lets go period 6
+db = conn['z-pd6']
+#Our storybook <3
+col = db["WinBall's StoryBook"]
+
+def newStory(name):
+	col.save({'name': name, 'text': ["Not Started Yet"]})
+
+def getStories():
+	stories = []
+	for line in col.find():
+ 		stories.append(line['name'])
+	return stories
+
+def getStory(name):
+        return col.find({'name':name})[0]
+
+def getText(story):
+	return getStory(story)['text']
+
+def addLine(story, newline):
+	col.update({'name': story}, {'$push': {'text': newline}})
+	return getStory(story)
+
+def dropone(story):
+	col.remove({'name': story})
+
+def dropall():
+	col.remove()
+
+#print addLine("asdf", "Hello!")
+
+
+#col.drop()
+#col.save({'name': "asdf", 'lines': 0, 'text': ["Not Yet Started"]})
+
+
