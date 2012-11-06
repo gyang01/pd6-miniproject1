8b13199b90fd47eef351ffc89f7e7d0e56052226
Sam Vasilevskiy <sam@sam-ThinkPad-X61.(none)>
2012-11-06 18:52:49 -0500

    mongo.py

---
+++ Sam-Kevin/mongo.py
@@ -0,0 +1,53 @@
+from pymongo import *
+
+global connection, db, res, collection
+
+def connect():
+     global connection, db, res, collection   
+     
+     connection = Connection('mongo.stuycs.org')
+     db = connection.admin
+     res = db.authenticate('ml7', 'ml7')
+     db = connection['z-pd6']
+     collection = db['sk']
+
+#    New story
+def addStory(title, line):    
+     global collection
+     collection.insert({'title': title, 'text': line})
+
+#    Returns a list of stories
+def getStories():
+     global collection
+     text = collection.find()
+     stories = []
+     for line in text:
+          story = line['title'].encode('utf8')
+          stories.append(story)
+     print stories
+     return stories
+
+#    Returns the text of a story
+def getText(story):
+     global collection
+     text = collection.find_one({'title': story})['text']
+     return text
+
+#    Adds a line to a story
+def addLine(story, line):
+     text = getText(story)
+     text.append(line)
+     collection.update({'title': story}, {'text': text})
+     
+connect()
+addStory('Story1','Sam eats an apple.')
+print 'STORY1:'
+print getText('Story1')
+addStory('Story2','The apple eats Sam.')
+print 'STORY2:'
+print getText('Story2')
+addLine('Story2','He was delicious.')
+getStories()
+getText('Story1')
+getText('Story2')
+collection.drop()
