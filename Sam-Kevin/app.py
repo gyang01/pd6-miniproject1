9f97decbe796fb601c9c2ad48db94c301298e507
Kevin <kevin@kevin-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-11-06 19:47:20 -0500

    stuff1

--- Sam-Kevin/app.py
+++ Sam-Kevin/app.py
@@ -1,6 +1,6 @@
 from flask import *
 
-import util
+#import util
 
 app = Flask(__name__)
 app.secret_key = 'blah1orblah2'
@@ -15,115 +15,18 @@ def login():
     if request.method=='GET':
         return render_template("login.html")
     else:
-        studentEmail=request.form['studentEmail']
-        studentID=request.form['studentID']
-        if(util.loginAuth(str(studentEmail),studentID)):
-            session['user'] = request.form['studentEmail']#.encode('ascii','replace')
-           # print session['user'] #debug
-            return redirect(url_for('home'))
-        else:
-            return redirect(url_for('error'))
+        return redirect(url_for('home'))
 
-@app.route("/logout/",methods=['GET','POST'])
-def logoff():
-    session.pop('user',None)
-    return redirect(url_for('login'))
-    
 
 @app.route("/home/",methods=['GET','POST'])
 def home():
     if request.method=='GET':
         return render_template("home.html")
     else:
-        option=request.form['option']
-        #if option == 'Rate':
-         #   return redirect(url_for('Rate'))
-        #if option == 'Your Ratings':
-         #   return redirect(url_for('Your_Ratings'))
-        if option == 'Logout':
+        button=request.form['button']
+        if button == 'Back':
             session.pop('user',None)
-            return redirect(url_for('login'))
-        pass
-
-@app.route("/rate/",methods=['GET','POST'])
-def Rate(yourGroup=None,questions=questions):
-    if request.method=='GET':
-        if 'user' in session:
-            try:
-                email = str(session['user'])
-                yourGroup = util.getGroup(email)
-                print yourGroup #debug
-                return render_template('rate.html',groupmembers=yourGroup,questions=questions)
-            except:
-                return render_template('rate.html',groupmembers=["default@github.com"],questions=questions)
-        else:
-            return redirect(url_for("login"))
-    else:
-        submit = request.form['submit']
- #       option = request.form['option']
-#        if submit == 'submit':
-        for i in util.getGroup(str(session['user'])):
-            for j in range(11):
-                k=str(j)
-                r = request.form[str(i+','+k)]
-                rate.append(r)
-                print r
-            util.giveRating(session['user'],i,rate)
-            rate = []
-#        radio = request.form['radio']
-#        for i in range (len(questions)):
-#            print radio[i]
-#            r = radio[i]
-        #    rate = rate + r
-        return redirect(url_for('home'))
-#        if option == 'Back':
-#            return render_template("home.html")
-        #if option == 'Logout':
-            #session.pop('user',None)
-            #return redirect(url_for('login'))
-
-
-@app.route("/yourRatings/",methods=['GET','POST'])
-def yourRatings():
-    if request.method=='GET':
-        try:
-            email = str(session['user'])
-            #print email
-            ratings = util.getAllRatings(email)
-            #print ratings
-            return render_template('yourRatings.html',ratings=ratings,questions=questions)
-        except:
-            try:
-                return render_template('yourRatings.html')
-            except:
-                return redirect(url_for("login"))
-        
-    else:
-        option=request.form['option']
-        if option=='Back':
-            return redirect(url_for('home'))
-        pass
-
-@app.route("/pastRatings/",methods=['GET','POST'])
-def pastRatings():
-    rated = None
-    if request.method=='GET':
-        if 'user' in session:
-            try:
-                email = str(session['user'])
-                rated = util.ratingsGiven(email) #causes a key error when given a string in ascii
-            except:
-                pass
-        return render_template('pastRatings.html',ratings=rated)
-    else:
-       option = request.form['option']
-       if option == 'Back':
-            return redirect(url_for('home'))
-       pass
-
-@app.route("/error/",methods=['GET','POST'])
-def error(name=None,last=None):
-    return render_template('error.html')
+            return redirect('/login/')
 
 if __name__=="__main__":
     app.debug=True
