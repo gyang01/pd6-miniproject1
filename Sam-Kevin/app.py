caf59ce884ad9276d6b5894eb83f9af2c5a63916
Kevin <kevin@kevin-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-11-06 21:17:46 -0500

    more stuff

--- Sam-Kevin/app.py
+++ Sam-Kevin/app.py
@@ -2,10 +2,11 @@ from flask import *
 
 #import mongo
 
+global Story
 app = Flask(__name__)
 app.secret_key = 'blah1orblah2'
 
-
+global story
 @app.route("/")
 def start():
     return redirect(url_for('login'))
@@ -13,15 +14,20 @@ def start():
 @app.route("/login/",methods=['GET','POST'])
 def login():
     if request.method=='GET':
-        return render_template("login.html")
+#        story = util.getStories;
+        return render_template("login.html")#,story=story)
     else:
-        return redirect(url_for('home'))
+        button=request.form['button']
+        if button == "GO":
+# story = Textfield[story]
+            return redirect('/home/'+story)
 
 
-@app.route("/home/",methods=['GET','POST'])
-def home():
+@app.route("/home/<story>/",methods=['GET','POST'])
+def home(story):
     if request.method=='GET':
-        return render_template("home.html")
+#        story=getStory();
+        return render_template("home.html")#,story=story)
     else:
         button=request.form['button']
         if button == 'Back':
