faf19c70b26e0833f68c3173189e7e73f4732a44
Michael Cohen <budjmt@aol.com>
2012-11-07 23:46:19 -0500

    Fixed app

--- Team_MACK/app.py
+++ Team_MACK/app.py
@@ -15,7 +15,8 @@ db = startConnection().mack
 def selection():
     if request.method == "POST":
         if "story_menu" in request.form:
-            return redirect(url_for("story_page", story=request.form["story_menu"]))
+            return redirect(url_for("story_page"
+                                    ,story = request.form["story_menu"]))
         else:
             story_name = request.form["add_story_field"]
             if story_name:
@@ -23,7 +24,7 @@ def selection():
     return render_template("select.html", stories=getStories(db))
 
 @app.route("/<story>", methods=["GET", "POST"])
-def story(story):
+def story_page(story):
     if request.method == "POST":
         addLine(db, story, request.form["add_line_field"])
     return render_template("story.html", lines=getStory(db, story))
